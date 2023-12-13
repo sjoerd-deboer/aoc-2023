a1ba6af967f98218f1c9126f35a1a0716abe6845
s2396041 <s.deboer-1@student.utwente.nl>
2023-12-13 10:36:49 +0100

    Day 7

--- day7.py
+++ day7.py
@@ -7,60 +7,70 @@ file_path = test_path if TEST else input_path
 with open(file_path, 'r') as f:
     data = [x.strip().split() for x in f.readlines()]
 
-CARD_DECK = '23456789TJQKA'
-TYPE_SCORE = len(CARD_DECK) + 1
 
-def get_rank_score(card):
-    return CARD_DECK.find(card) + 1
+def get_rank_score(card: str, jokers: bool = False) -> int:
+    return 'J23456789TJQKA'.find(card) + 1 if jokers else '23456789TJQKA'.find(card) + 1
 
 
-def part_one():
+def determine_hand_type(hand: str, jokers: bool = False) -> tuple:
+    high_card = [get_rank_score(x, jokers) for x in hand]
+    number_of_jokers = hand.count('J')
+    hand = {x: sum(y == x for y in hand) for x in hand}
+    symbols, amount = list(hand.keys()), list(hand.values())
+    hand_size = len(symbols)
+    if hand_size == 1:
+        return 6, high_card
+    elif hand_size == 2:
+        if 4 in amount:
+            if jokers and number_of_jokers in [1, 4]:
+                return 6, high_card
+            else:
+                return 5, high_card
+        elif 3 in amount and 2 in amount:
+            if jokers and number_of_jokers == 2:
+                return 6, high_card
+            elif jokers and number_of_jokers == 3:
+                return 6, high_card
+            else:
+                return 4, high_card
+    elif hand_size == 3:
+        if 3 in amount:
+            if jokers and number_of_jokers in [1, 3]:
+                return 5, high_card
+            else:
+                return 3, high_card
+        else:
+            if jokers and number_of_jokers == 1:
+                return 4, high_card
+            elif jokers and number_of_jokers == 2:
+                return 5, high_card
+            else:
+                return 2, high_card
+    elif hand_size == 4:
+        if jokers and number_of_jokers in [1, 2]:
+            return 3, high_card
+        else:
+            return 1, high_card
+    elif hand_size == 5:
+        if jokers and number_of_jokers == 1:
+            return 1, high_card
+        else:
+            return 0, high_card
+
+
+def part_one(jokers: bool = False) -> int:
     result = []
     for h in data:
         hand, bid = h
-        hand = {x: sum(y == x for y in hand) for x in hand}
-        symbols, amount = list(hand.keys()), list(hand.values())
-        hand = list(zip(symbols, amount))
-        hand_size = len(hand)
-        print(h[0], hand)
-        score, score2, score3 = 0, 0, 0
-        if hand_size == 1:
-            print("Five of a kind")
-            score = (6 * TYPE_SCORE) + get_rank_score(hand[0][0])
-        elif hand_size == 2:
-            if amount.count(4):
-                print('Four of a kind')
-                score = (5 * TYPE_SCORE) + get_rank_score(symbols[amount.index(4)])
-                score2 = get_rank_score(symbols[amount.index(1)])
-            else:
-                print('Full house')
-                average_rank = (get_rank_score(symbols[amount.index(3)]) + get_rank_score(symbols[amount.index(2)]))/2
-                score = (4 * TYPE_SCORE) + average_rank
-                score2 = get_rank_score(symbols[amount.index(2)])
-        elif hand_size == 3:
-            if amount.count(3):
-                print('Three of a kind')
-                score = (3 * TYPE_SCORE) + get_rank_score(symbols[amount.index(3)])
-                # TODO: Fix this.
-                score2 = get_rank_score(symbols[amount.index(1)])
-            else:
-                print('Two pair')
-                average_rank = [get_rank_score(a) for a, b in hand if b == 2]
-                score = (2 * TYPE_SCORE) + min(average_rank)/4 + max(average_rank)/2
-        elif hand_size == 4:
-            print('One pair')
-            score = (1 * TYPE_SCORE) + get_rank_score(symbols[amount.index(2)])
-        elif hand_size == 5:
-            score = max([get_rank_score(x) for x in h[0]])
-        result.append([h[0], score, bid])
-    print(result)
-    result.sort(key=lambda x: x[0])
-    return sum([(x+1) * int(result[x][2]) for x in range(len(result))])
+        score = determine_hand_type(hand, jokers)
+        result.append([score[0], score[1], int(bid)])
+    hand_ranks = sorted(result, key=lambda x: (x[0], x[1]), reverse=True)
+    scores = [hand_ranks[x][2] * (len(hand_ranks)-x) for x in range(len(hand_ranks))]
+    return sum(scores)
 
 
-def part_two():
-    result = data
-    return result
+def part_two() -> int:
+    return part_one(jokers=True)
 
 
 if __name__ == '__main__':
